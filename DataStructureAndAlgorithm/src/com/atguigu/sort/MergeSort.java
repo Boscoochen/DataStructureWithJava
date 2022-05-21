36cb854aa4df5725c022b967280d7233de179203
Boscoochen <boscochen203@gmail.com>
2022-05-20 23:10:10 -0500

    add timer to merge sort

--- DataStructureAndAlgorithm/src/com/atguigu/sort/MergeSort.java
+++ DataStructureAndAlgorithm/src/com/atguigu/sort/MergeSort.java
@@ -1,13 +1,27 @@
 package com.atguigu.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int arr[] = {8, 4, 5, 7, 1, 3, 6, 2};
+//        int arr[] = {8, 4, 5, 7, 1, 3, 6, 2};
+//        int temp[] = new int[arr.length];
+        int[] arr = new int[8000000];
         int temp[] = new int[arr.length];
+        for (int i = 0; i < 8000000; i++) {
+            arr[i] = (int)(Math.random() * 8000000);
+        }
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1str = simpleDateFormat.format(date);
+        System.out.println(date1str);
         mergeSort(arr, 0, arr.length - 1, temp);
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
+        Date date2 = new Date();
+        String date2str = simpleDateFormat.format(date2);
+        System.out.println(date2str);
     }
 
     public static void mergeSort(int[] arr, int left, int right, int[] temp) {

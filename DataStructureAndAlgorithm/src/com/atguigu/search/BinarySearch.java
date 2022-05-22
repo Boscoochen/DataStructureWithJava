dcbf0759549e4f17c3d188fb683329e59ba73877
Boscoochen <boscochen203@gmail.com>
2022-05-22 00:07:12 -0500

    optimize binary search

--- DataStructureAndAlgorithm/src/com/atguigu/search/BinarySearch.java
+++ DataStructureAndAlgorithm/src/com/atguigu/search/BinarySearch.java
@@ -1,10 +1,15 @@
 package com.atguigu.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BinarySearch {
     public static void main(String[] args) {
-        int arr[] = {1, 8, 10, 89, 1000, 1234};
+        int arr[] = {1, 8, 10, 89, 1000, 1000, 1000, 1000, 1234};
 
-        int resIndex = binarySearch(arr, 0, arr.length - 1, 88);
+//        int resIndex = binarySearch(arr, 0, arr.length - 1, 88);
+//        System.out.println("resIndex " + resIndex);
+        ArrayList<Integer> resIndex = binarySearch2(arr, 0, arr.length - 1, 1000);
         System.out.println("resIndex " + resIndex);
     }
 
@@ -20,6 +25,40 @@ public class BinarySearch {
             return binarySearch(arr, left, mid - 1, findVal);
         }else {
             return mid;
+        }
+    }
+
+    public static ArrayList<Integer> binarySearch2(int[] arr, int left, int right, int findVal) {
+        if(left > right) {
+            return new ArrayList<Integer>();
+        }
+        int mid = (left + right) / 2;
+        int midVal = arr[mid];
+        if(findVal > midVal) {
+            return binarySearch2(arr, mid + 1, right, findVal);
+        }else if(findVal < midVal) {
+            return binarySearch2(arr, left, mid - 1, findVal);
+        }else {
+            ArrayList<Integer> resultIndexList = new ArrayList<>();
+            int temp = mid - 1;
+            while(true) {
+                if(temp < 0 || arr[temp] != findVal) {
+                    break;
+                }
+                resultIndexList.add(temp);
+                temp -= 1;
+            }
+            resultIndexList.add(mid);
+
+            temp = mid + 1;
+            while(true) {
+                if(temp > arr.length - 1 || arr[temp] != findVal) {
+                    break;
+                }
+                resultIndexList.add(temp);
+                temp += 1;
+            }
+            return resultIndexList;
         }
     }
 }

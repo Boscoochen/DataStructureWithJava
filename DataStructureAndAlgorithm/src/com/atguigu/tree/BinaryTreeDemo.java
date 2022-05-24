953d38728b5d149a8d614496fc18a8eb3e0e1927
Boscoochen <boscochen203@gmail.com>
2022-05-24 11:29:35 -0500

    add preorder,infixorder,postorder tree

---
+++ DataStructureAndAlgorithm/src/com/atguigu/tree/BinaryTreeDemo.java
@@ -0,0 +1,124 @@
+package com.atguigu.tree;
+
+public class BinaryTreeDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+class BinaryTree {
+    private HeroNode root;
+
+    public void setRoot(HeroNode root) {
+        this.root = root;
+    }
+
+    public void preOrder() {
+        if (this.root != null) {
+            this.root.preOrder();
+        } else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    public void infixOrder() {
+        if (this.root != null) {
+            this.root.infixOder();
+        } else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    public void postOrder() {
+        if (this.root != null) {
+            this.root.postOrder();
+        } else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+}
+
+
+class HeroNode {
+    private int no;
+    private String name;
+    private HeroNode left;
+    private HeroNode right;
+
+    public HeroNode(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HeroNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(HeroNode left) {
+        this.left = left;
+    }
+
+    public HeroNode getRight() {
+        return right;
+    }
+
+    public void setRight(HeroNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public void preOrder() {
+        System.out.println(this);
+        if (this.left != null) {
+            this.left.preOrder();
+        }
+        if (this.right != null) {
+            this.right.preOrder();
+        }
+    }
+
+    public void infixOder() {
+        if (this.left != null) {
+            this.left.infixOder();
+        }
+        System.out.println(this);
+        if (this.right != null) {
+            this.right.infixOder();
+        }
+    }
+
+    public void postOrder() {
+        if (this.left != null) {
+            this.left.postOrder();
+        }
+
+        if (this.right != null) {
+            this.right.postOrder();
+        }
+
+        System.out.println(this);
+    }
+}

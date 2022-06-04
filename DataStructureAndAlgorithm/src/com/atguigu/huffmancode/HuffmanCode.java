35864f557ff61eccf2014e85b623f475020d54ff
Boscoochen <boscochen203@gmail.com>
2022-06-04 00:29:43 -0500

    update huffman tree compress string

--- DataStructureAndAlgorithm/src/com/atguigu/huffmancode/HuffmanCode.java
+++ DataStructureAndAlgorithm/src/com/atguigu/huffmancode/HuffmanCode.java
@@ -1,15 +1,26 @@
 package com.atguigu.huffmancode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class HuffmanCode {
     public static void main(String[] args) {
         String content = "i like like like java do you like a java";
         byte[] contentBytes = content.getBytes();
         System.out.println(contentBytes.length);
+        List<Node> nodes = getNodes(contentBytes);
+        System.out.println("nodes=" + nodes);
+        System.out.println("霍夫曼树");
+        Node huffmanTree = createHuffmanTree(nodes);
+        System.out.println("前序遍历");
+        huffmanTree.preOrder();
+    }
+
+    private static void preOrder(Node root) {
+        if(root != null) {
+            root.preOrder();
+        }else {
+            System.out.println("霍夫曼树为空");
+        }
     }
 
     private static List<Node> getNodes(byte[] bytes) {
@@ -23,6 +34,27 @@ public class HuffmanCode {
                 counts.put(b, count + 1);
             }
         }
+
+        for(Map.Entry<Byte, Integer> entry : counts.entrySet()) {
+            nodes.add(new Node(entry.getKey(), entry.getValue()));
+        }
+
+        return nodes;
+    }
+
+    private static Node createHuffmanTree(List<Node> nodes) {
+        while(nodes.size() > 1) {
+            Collections.sort(nodes);
+            Node leftNode = nodes.get(0);
+            Node rightNode = nodes.get(1);
+            Node parent = new Node(null, leftNode.weight + rightNode.weight);
+            parent.left = leftNode;
+            parent.right = rightNode;
+            nodes.remove(leftNode);
+            nodes.remove(rightNode);
+            nodes.add(parent);
+        }
+        return nodes.get(0);
     }
 }
 

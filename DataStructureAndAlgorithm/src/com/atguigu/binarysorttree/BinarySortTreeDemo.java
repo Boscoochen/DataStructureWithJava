c0d874cd4878bda4eaf591ea0f3f2951b97af2e8
Boscoochen <boscochen203@gmail.com>
2022-06-08 23:54:52 -0500

    add binary sort tree algorithm

---
+++ DataStructureAndAlgorithm/src/com/atguigu/binarysorttree/BinarySortTreeDemo.java
@@ -0,0 +1,23 @@
+package com.atguigu.binarysorttree;
+
+public class BinarySortTreeDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public void add(Node node) {
+        if(node == null) {
+            return;
+        }
+    }
+}

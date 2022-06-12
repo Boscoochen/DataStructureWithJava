6308d966fedcf3aba3ad7c84aa7570359b320446
Boscoochen <boscochen203@gmail.com>
2022-06-11 23:42:50 -0500

    updates binary sort tree

--- DataStructureAndAlgorithm/src/com/atguigu/binarysorttree/BinarySortTreeDemo.java
+++ DataStructureAndAlgorithm/src/com/atguigu/binarysorttree/BinarySortTreeDemo.java
@@ -4,9 +4,32 @@ import java.util.ArrayList;
 
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
+        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        BinarySortTree binarySortTree = new BinarySortTree();
+        for (int i = 0; i < arr.length; i++) {
+            binarySortTree.add(new Node(arr[i]));
+        }
+        System.out.println("中序遍历二叉排序树");
+        binarySortTree.infixOrder();
+    }
+}
 
+class BinarySortTree {
+    private Node root;
+    public void add(Node node) {
+        if(root == null) {
+            root = node;
+        } else {
+            root.add(node);
+        }
+    }
 
-
+    public void infixOrder() {
+        if(root != null) {
+            root.infixOrder();
+        }else {
+            System.out.println("二叉排序树为空， 不能遍历");
+        }
     }
 }
 
@@ -20,9 +43,39 @@ class Node {
     }
 
     public void add(Node node) {
-        if(node == null) {
+        if (node == null) {
             return;
         }
+        if (node.value < this.value) {
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                this.left.add(node);
+            }
+        } else {
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                this.right.add(node);
+            }
+        }
+    }
+
+    public void infixOrder() {
+        if (this.left != null) {
+            this.left.infixOrder();
+        }
+        System.out.println(this);
+        if (this.right != null) {
+            this.right.infixOrder();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                '}';
     }
 }
 

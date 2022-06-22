58777995972f6d6bfd2aa79bc9ddbb747b1a3d3a
Boscoochen <boscochen203@gmail.com>
2022-06-22 15:41:50 -0500

    updates

---
+++ DataStructureAndAlgorithm/src/com/Algorithm/greedy/GreedyAlgorithm.java
@@ -0,0 +1,11 @@
+package com.Algorithm.greedy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class GreedyAlgorithm {
+    public static void main(String[] args) {
+        HashMap<String, HashSet<String>> broadcast = new HashMap<String, HashSet<String>>();
+
+    }
+}

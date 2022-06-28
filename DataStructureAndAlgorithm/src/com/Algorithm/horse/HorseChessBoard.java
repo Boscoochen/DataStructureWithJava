89084a37928c95124ab51c7fd47f5440d922200f
Boscoochen <boscochen203@gmail.com>
2022-06-28 13:09:53 -0500

    update horse chess board algorithm

--- DataStructureAndAlgorithm/src/com/Algorithm/horse/HorseChessBoard.java
+++ DataStructureAndAlgorithm/src/com/Algorithm/horse/HorseChessBoard.java
@@ -6,9 +6,47 @@ import java.util.ArrayList;
 public class HorseChessBoard {
     private static int X;
     private static int Y;
+    private static boolean visited[];
+    private static boolean finished;
 
     public static void main(String[] args) {
+        X = 8;
+        Y = 8;
+        int row = 1;
+        int column = 1;
 
+        int[][] chessboard = new int[X][Y];
+        visited = new boolean[X * Y];
+        long start = System.currentTimeMillis();
+        traversalChessBoard(chessboard, row - 1, column - 1, 1);
+        long end = System.currentTimeMillis();
+        System.out.println("共耗时：" + (end - start) + " 毫秒");
+
+        for(int[] rows : chessboard) {
+            for(int step: rows) {
+                System.out.print(step + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void traversalChessBoard(int[][] chessboard, int row, int column, int step) {
+        chessboard[row][column] = step;
+        visited[row * X + column] = true;
+        ArrayList<Point> ps = next(new Point(column, row));
+        while (!ps.isEmpty()) {
+            Point p = ps.remove(0);
+            if (!visited[p.y * X + p.x]) {
+                traversalChessBoard(chessboard, p.y, p.x, ++step);
+            }
+        }
+
+        if (step < X * Y && !finished) {
+            chessboard[row][column] = 0;
+            visited[row * X + column] = false;
+        } else {
+            finished = true;
+        }
     }
 
     public static ArrayList<Point> next(Point curPoint) {

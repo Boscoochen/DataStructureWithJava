b9a27a472925c4cf50a3ac49578b1fc36bde19e9
Boscoochen <boscochen203@gmail.com>
2022-06-28 12:11:13 -0500

    add horse chess board algorithm

---
+++ DataStructureAndAlgorithm/src/com/Algorithm/horse/HorseChessBoard.java
@@ -0,0 +1,45 @@
+package com.Algorithm.horse;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class HorseChessBoard {
+    private static int X;
+    private static int Y;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ArrayList<Point> next(Point curPoint) {
+        ArrayList<Point> ps = new ArrayList<Point>();
+        Point p1 = new Point();
+        if((p1.x = curPoint.x - 2) >= 0 && (p1.y = curPoint.y - 1) >= 0) {
+            ps.add(new Point(p1));
+        }
+        if ((p1.x = curPoint.x - 1) >= 0 && (p1.y = curPoint.y-2) >= 0) {
+            ps.add(new Point(p1));
+        }
+        if ((p1.x = curPoint.x + 1) < X && (p1.y = curPoint.y-2) >= 0) {
+            ps.add(new Point(p1));
+        }
+        if ((p1.x = curPoint.x + 2) < X && (p1.y = curPoint.y-1) >= 0) {
+            ps.add(new Point(p1));
+        }
+        if ((p1.x = curPoint.x + 2) < X && (p1.y = curPoint.y+1) < Y) {
+            ps.add(new Point(p1));
+        }
+        if ((p1.x = curPoint.x + 1) < X && (p1.y = curPoint.y+2) < Y) {
+            ps.add(new Point(p1));
+        }
+        if ((p1.x = curPoint.x - 1) >= 0 && (p1.y = curPoint.y+2) < Y) {
+            ps.add(new Point(p1));
+        }
+        if ((p1.x = curPoint.x - 2) >= 0 && (p1.y = curPoint.y+1) < Y) {
+            ps.add(new Point(p1));
+        }
+        return ps;
+    }
+}
+
+

86204ffcdd20628492f3ae4938e729196f1fca84
Boscoochen <boscochen203@gmail.com>
2022-06-30 18:25:34 -0500

    debug hanoi tower

--- DataStructureAndAlgorithm/src/com/Algorithm/divideandconquer/HanoiTower.java
+++ DataStructureAndAlgorithm/src/com/Algorithm/divideandconquer/HanoiTower.java
@@ -2,7 +2,7 @@ package com.Algorithm.divideandconquer;
 
 public class HanoiTower {
     public static void main(String[] args) {
-        hanoiTower(2, 'A', 'B', 'C');
+        hanoiTower(3, 'A', 'B', 'C');
     }
 
     public static void hanoiTower(int num, char a, char b, char c) {
@@ -15,3 +15,22 @@ public class HanoiTower {
         }
     }
  }
+
+
+
+
+
+
+//hanoiTower(1, A, B, C)
+//hanoiTower(2, B, A, C)
+//hanoiTower(3, A, B, C)
+
+
+//output
+//第一个盘从A->C
+//第2个盘从A->B
+//第一个盘从C->B
+//第3个盘从A->C
+//第一个盘从B->A
+//第2个盘从B->C
+//第一个盘从A->C

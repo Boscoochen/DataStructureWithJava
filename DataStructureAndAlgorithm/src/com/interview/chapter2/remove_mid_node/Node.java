163c3f5a227bbe4247d270f7c9b0fdff58e52cf0
Boscoochen <boscochen203@gmail.com>
2022-07-19 12:52:41 -0500

    add remove middle node

---
+++ DataStructureAndAlgorithm/src/com/interview/chapter2/remove_mid_node/Node.java
@@ -0,0 +1,10 @@
+package com.interview.chapter2.remove_mid_node;
+
+public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}
---
+++ DataStructureAndAlgorithm/src/com/interview/chapter2/remove_mid_node/RemoveMidNode.java
@@ -0,0 +1,74 @@
+package com.interview.chapter2.remove_mid_node;
+
+import java.util.regex.Pattern;
+
+public class RemoveMidNode {
+    public static Node head;
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+
+        head = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        print();
+//        head = removeMidNode(head);
+        head = removeByRatio(head, 4, 5);
+        print();
+    }
+    public static Node removeMidNode(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        if (head.next.next == null) {
+            return head.next;
+        }
+        Node pre = head;
+        Node cur = head.next.next;
+        while (cur.next != null && cur.next.next != null) {
+            pre = pre.next;
+            cur = cur.next.next;
+        }
+        pre.next = pre.next.next;
+        return head;
+    }
+
+    public static Node removeByRatio(Node head, int a, int b) {
+        if (a < 1 || a > b) {
+            return head;
+        }
+        int n = 0;
+        Node cur = head;
+        while (cur != null) {
+            n++;
+            cur = cur.next;
+        }
+        n = (int) Math.ceil((double) (a * n) / (double) b);
+        if (n == 1) {
+            head = head.next;
+        }
+        if (n > 1) {
+            cur = head;
+            while (--n != 1) {
+                cur = cur.next;
+            }
+            cur.next = cur.next.next;
+        }
+        return head;
+    }
+
+    public static void print() {
+        Node cur = head;
+        while (cur != null) {
+            System.out.print(cur.value + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+}

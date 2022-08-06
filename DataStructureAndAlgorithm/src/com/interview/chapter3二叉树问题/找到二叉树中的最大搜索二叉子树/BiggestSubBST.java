1bd64de3912de5b2ca14ca59fa73ec71f9eb7729
Boscoochen <boscochen203@gmail.com>
2022-08-05 20:06:47 -0500

    add BiggestSubBST

---
+++ DataStructureAndAlgorithm/src/com/interview/chapter3二叉树问题/找到二叉树中的最大搜索二叉子树/BiggestSubBST.java
@@ -0,0 +1,65 @@
+package com.interview.chapter3二叉树问题.找到二叉树中的最大搜索二叉子树;
+
+public class BiggestSubBST {
+    public static void main(String[] args) {
+        Node head = new Node(6);
+        head.left = new Node(1);
+        head.left.left = new Node(0);
+        head.left.right = new Node(3);
+        head.right = new Node(12);
+        head.right.left = new Node(10);
+        head.right.left.left = new Node(4);
+        head.right.left.left.left = new Node(2);
+        head.right.left.left.right = new Node(5);
+        head.right.left.right = new Node(14);
+        head.right.left.right.left = new Node(11);
+        head.right.left.right.right = new Node(15);
+        head.right.right = new Node(13);
+        head.right.right.left = new Node(20);
+        head.right.right.right = new Node(16);
+        Node head2 = biggestSubBst(head);
+        print(head2);
+    }
+
+    public static Node biggestSubBst(Node head) {
+        int[] record = new int[3];
+        return posOrder(head, record);
+    }
+
+    public static Node posOrder(Node head, int[] record) {
+        if (head == null) {
+            record[0] = 0;
+            record[1] = Integer.MAX_VALUE;
+            record[2] = Integer.MIN_VALUE;
+            return null;
+        }
+        int value = head.value;
+        Node left = head.left;
+        Node right = head.right;
+        Node lBST = posOrder(left, record);
+        int lSize = record[0];
+        int lMin = record[1];
+        int lMax = record[2];
+        Node rBST = posOrder(right, record);
+        int rSize = record[0];
+        int rMin = record[1];
+        int rMax = record[2];
+        record[1] = Math.min(lMin, value);
+        record[2] = Math.max(rMax, value);
+        if (left == lBST && right == rBST && lMax < value && value < rMin) {
+            record[0] = lSize + rSize + 1;
+            return head;
+        }
+        record[0] = Math.max(lSize, rSize);
+        return lSize > rSize ? lBST : rBST;
+    }
+
+    public static void print(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.value + " ");
+        print(head.left);
+        print(head.right);
+    }
+}
---
+++ DataStructureAndAlgorithm/src/com/interview/chapter3二叉树问题/找到二叉树中的最大搜索二叉子树/Node.java
@@ -0,0 +1,11 @@
+package com.interview.chapter3二叉树问题.找到二叉树中的最大搜索二叉子树;
+
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}

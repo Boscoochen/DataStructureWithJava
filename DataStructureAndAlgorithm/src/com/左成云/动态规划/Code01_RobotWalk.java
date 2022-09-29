bbeabd7da59b7eee613eea87d4db019cbf17c3df
Boscoochen <boscochen203@gmail.com>
2022-09-28 21:56:12 -0500

    动态规划优化

--- DataStructureAndAlgorithm/src/com/左成云/动态规划/Code01_RobotWalk.java
+++ DataStructureAndAlgorithm/src/com/左成云/动态规划/Code01_RobotWalk.java
@@ -28,7 +28,41 @@ public class Code01_RobotWalk {
         return process1(cur - 1, rest - 1, aim, N) + process1(cur + 1, rest - 1, aim, N);
     }
 
+    public static int way2(int N, int start, int aim, int K) {
+        int[][] dp = new int[N+1][K+1];
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= K; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return process2(start, K, aim, N, dp);
+    }
+
+    // cur 范围：1~N
+    // rest 范围：0～k
+    public static int process2(int cur, int rest, int aim, int N, int[][] dp) {
+        if (dp[cur][rest] != -1) {
+            return dp[cur][rest];
+        }
+
+        int ans = 0;
+
+        if (rest == 0) {
+            ans = cur == aim ? 1 : 0;
+        } else if (cur == 1) {
+            ans = process2(2, rest - 1, aim, N, dp);
+        } else if (cur == N) {
+            ans = process2(N - 1, rest - 1, aim, N, dp);
+        } else {
+            ans = process2(cur - 1, rest - 1, aim, N, dp)
+                    + process2(cur + 1, rest - 1, aim, N, dp);
+        }
+
+        dp[cur][rest] = ans;
+        return ans;
+    }
     public static void main(String[] args) {
-        System.out.println(ways(4, 2,4,4));
+        System.out.println(way2(4, 2,4,4));
     }
 }

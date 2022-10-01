4fb0b159cd5f74be01fba3af2fd0a4ba94242fd7
Boscoochen <boscochen203@gmail.com>
2022-10-01 11:17:08 -0500

    矩阵最小路径和

--- DataStructureAndAlgorithm/src/com/interview/chapter4递归和动态规划/矩阵的最小路径和/MinPathSum.java
+++ DataStructureAndAlgorithm/src/com/interview/chapter4递归和动态规划/矩阵的最小路径和/MinPathSum.java
@@ -14,17 +14,40 @@ public class MinPathSum {
             }
             System.out.println();
         }
-        System.out.println(Fibonacci(5));
+
+        System.out.println(way1(0, 0, m));
     }
 
-    public static int Fibonacci(int n)
-    {
-        if(n == 1 || n == 2)
-        {
-            return n;
+    public static int way1(int row, int col, int[][] m) {
+        return process1(row, col, m);
+    }
+
+    public static int process1(int row, int col, int[][] m) {
+        //base case
+        if (row == m.length - 1 && col == m[0].length - 1) {
+            return m[row][col];
         }
-        return Fibonacci(n-1) + Fibonacci(n-2);
+
+        if (row == m.length - 1) {
+            return m[row][col] + process1(row, col + 1, m);
+        }
+
+        if (col == m[0].length - 1) {
+            return m[row][col] + process1(row + 1, col, m);
+        }
+
+        int down = m[row][col] + process1(row + 1, col, m);
+        int right = m[row][col] + process1(row, col + 1, m);
+        return Math.min(down, right);
+//        return m[row][col] + Math.min(process1(row+ 1, col, m), process1(row, col+1, m));
     }
 
+    /**
+     * {1,2,5,9},
+     * {8,1,3,4},
+     * {5,0,6,1},
+     * {8,8,4,0}
+     *
+     */
 
 }
--- DataStructureAndAlgorithm/src/com/左成云/动态规划/Code01_RobotWalk.java
+++ DataStructureAndAlgorithm/src/com/左成云/动态规划/Code01_RobotWalk.java
@@ -1,7 +1,7 @@
 package com.左成云.动态规划;
 
 public class Code01_RobotWalk {
-    public static int ways(int N, int start, int aim, int K) {
+    public static int ways1(int N, int start, int aim, int K) {
         return process1(start, K, aim, N);
     }
 
@@ -28,7 +28,7 @@ public class Code01_RobotWalk {
         return process1(cur - 1, rest - 1, aim, N) + process1(cur + 1, rest - 1, aim, N);
     }
 
-    public static int way2(int N, int start, int aim, int K) {
+    public static int ways2(int N, int start, int aim, int K) {
         int[][] dp = new int[N+1][K+1];
         for (int i = 0; i <= N; i++) {
             for (int j = 0; j <= K; j++) {
@@ -36,6 +36,10 @@ public class Code01_RobotWalk {
             }
         }
 
+        // dp就是缓存表
+        // dp[cur][rest] == -1 -> process2(cur, rest)之前没算过
+        // dp[cur][rest] != -1 -> process2(cur, rest)之前算过， 返回值就是 dp[cur][rest]
+        // N+1 * K+1
         return process2(start, K, aim, N, dp);
     }
 
@@ -62,7 +66,49 @@ public class Code01_RobotWalk {
         dp[cur][rest] = ans;
         return ans;
     }
+
+    public static int ways3(int N, int start, int aim, int K) {
+        int[][] dp = new int[N+1][K+1];
+        dp[aim][0] = 1; // dp[...][0] = 0
+        /**
+         * 自己的版本
+        for (int col = 1; col <= K; col++) {
+            for (int row = 1; row <= N; row++) {
+                if (row == 1) {
+                    dp[row][col] = dp[2][col-1];
+                } else if (row == N) {
+                    dp[row][col] = dp[row-1][col-1];
+                } else {
+                    dp[row][col] = dp[row-1][col-1] + dp[row+1][col-1];
+                }
+            }
+        }
+         */
+
+        for (int col = 1; col <= K; col++) {
+            dp[1][col] = dp[2][col-1];
+            for (int row = 2; row < N; row++) {
+                dp[row][col] = dp[row-1][col-1] + dp[row+1][col-1];
+            }
+            dp[N][col] = dp[N-1][col-1];
+        }
+
+        /**
+         * 打印缓存表
+         */
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= K; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return dp[start][K];
+    }
+
     public static void main(String[] args) {
-        System.out.println(way2(4, 2,4,4));
+//        System.out.println(way2(5, 2,4,6));
+//        System.out.println(ways2(6, 3, 6, 9));
+        System.out.println(ways3(5, 2,4,6));
     }
 }

f0018d0c16aa0a5f910c0e3906eb1725408f6b4c
Boscoochen <boscochen203@gmail.com>
2022-11-01 22:41:34 -0500

    updates code

--- DataStructureAndAlgorithm/src/com/左成云/基础提升_有序表_并查表/Code04_UnionFind.java
+++ DataStructureAndAlgorithm/src/com/左成云/基础提升_有序表_并查表/Code04_UnionFind.java
@@ -2,6 +2,7 @@ package com.左成云.基础提升_有序表_并查表;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
 public class Code04_UnionFind {
 
@@ -33,12 +34,37 @@ public class Code04_UnionFind {
             }
         }
 
-        public boolean isSameSet(V a, V b) {
+        private Element<V> findHead(Element<V> element) {
+            Stack<Element<V>> path = new Stack<>();
+            while (element != fatherMap.get(element)) {
+                path.push(element);
+                element = fatherMap.get(element);
+            }
+            while (!path.isEmpty()) {
+                fatherMap.put(path.pop(), element);
+            }
+            return element;
+        }
 
+        public boolean isSameSet(V a, V b) {
+            if (elementMap.containsKey(a) && elementMap.containsKey(b)) {
+                return findHead(elementMap.get(a)) == findHead(elementMap.get(b));
+            }
+            return false;
         }
 
         public void union(V a, V b) {
-
+            if (elementMap.containsKey(a) && elementMap.containsKey(b)) {
+                Element<V> aF = findHead(elementMap.get(a));
+                Element<V> bF = findHead(elementMap.get(b));
+                if (aF != bF) {
+                    Element<V> big = sizeMap.get(aF) >= sizeMap.get(bF) ? aF : bF;
+                    Element<V> small = big == aF ? bF : aF;
+                    fatherMap.put(small, big);
+                    sizeMap.put(big, sizeMap.get(aF) + sizeMap.get(bF));
+                    sizeMap.remove(small);
+                }
+            }
         }
     }
 }

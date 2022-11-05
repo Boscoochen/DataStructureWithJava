09f7fa7d6d5d09e1337ba58fdcd04e603843a7ae
Boscoochen <boscochen203@gmail.com>
2022-11-04 23:26:15 -0500

    updates code

--- DataStructureAndAlgorithm/src/com/左成云/基础提升_有序表_并查表/KMP.java
+++ DataStructureAndAlgorithm/src/com/左成云/基础提升_有序表_并查表/KMP.java
@@ -4,4 +4,33 @@ public class KMP {
     public static void main(String[] args) {
 
     }
+
+    public static int getIndexOf(String s, String m) {
+        if (s == null || m == null || m.length() < 1 || s.length() < m.length()) {
+            return -1;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = m.toCharArray();
+        int i1 = 0;
+        int i2 = 0;
+        int[] next = getNextArray(str2);
+
+        while (i1 < str1.length && i2 < str2.length) {
+            if (str1[i1] == str2[i2]) {
+                i1++;
+                i2++;
+            } else if (next[i2] == -1) {
+                i1++;
+            } else {
+                i2 = next[i2];
+            }
+        }
+        return i2 == str2.length ? i1 - i2 : -1;
+    }
+
+    public static int[] getNextArray(char[] ms) {
+        if (ms.length == 1) {
+
+        }
+    }
 }

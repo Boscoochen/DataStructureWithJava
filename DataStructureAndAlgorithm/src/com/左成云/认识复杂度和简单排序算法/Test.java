141ab82ce226e424374828d6b006bd2181d703f1
Boscoochen <boscochen203@gmail.com>
2022-11-08 23:29:40 -0600

    updates code

--- DataStructureAndAlgorithm/src/com/左成云/认识复杂度和简单排序算法/Test.java
+++ DataStructureAndAlgorithm/src/com/左成云/认识复杂度和简单排序算法/Test.java
@@ -1,9 +1,15 @@
 package com.左成云.认识复杂度和简单排序算法;
 
 public class Test {
+    static int[] res = new int[2];
     public static void main(String[] args) {
         int[] nums = {1,2,1,1,2,3,3};
+        int[] nums2 = {1,2,1,1,2,3,3,2};
         System.out.println(findOddNumber(nums));
+        find2OddNumber(nums2);
+        for (int i = 0; i < 2; i++) {
+            System.out.println(res[i]);
+        }
     }
 
     public static int findOddNumber(int[] nums) {
@@ -12,5 +18,22 @@ public class Test {
             eor = eor ^ nums[i];
         }
         return eor;
+    }
+    public static void find2OddNumber(int[] nums) {
+        int eor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            eor ^= nums[i];
+        }
+        int eorPi = 0;
+        eorPi = eor & (~eor + 1);
+
+        int eor2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if ((eorPi & nums[i]) == 0) {
+                eor2 ^= nums[i];
+            }
+        }
+        res[0] = eor2;
+        res[1] = eor2 ^ eor;
     }
 }

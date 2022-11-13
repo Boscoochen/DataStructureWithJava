1cb8179a2c15a946cf5855719ed2cd2ef88641dd
Boscoochen <boscochen203@gmail.com>
2022-11-12 23:00:50 -0600

    updated code

--- DataStructureAndAlgorithm/src/com/左成云/认识复杂度和简单排序算法/Code02_InsertionSort.java
+++ DataStructureAndAlgorithm/src/com/左成云/认识复杂度和简单排序算法/Code02_InsertionSort.java
@@ -2,7 +2,7 @@ package com.左成云.认识复杂度和简单排序算法;
 
 public class Code02_InsertionSort {
     public static void main(String[] args) {
-        int[] nums = {3,2,4,5,2,3,3};
+        int[] nums = {3, 2, 4, 5, 2, 3, 1};
         insertionSort(nums);
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
@@ -10,13 +10,9 @@ public class Code02_InsertionSort {
     }
 
     public static void insertionSort(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if (nums[j] < nums[j - 1]) {
-                    swap(nums,j, j - 1);
-                } else {
-                    break;
-                }
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0 && nums[j] > nums[j + 1]; j--) {
+                swap(nums, j, j + 1);
             }
         }
     }
